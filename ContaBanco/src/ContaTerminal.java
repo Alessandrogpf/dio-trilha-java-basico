61d7c711e5d2fdb44eeb1e42ee77fa81040516bf
Alessandro Gabriel Pena Furtado <alessandrogpf@hotmail.com>
2024-09-19 10:52:46 -0400

    Classe ContaBanco finalizada

--- ContaBanco/src/ContaTerminal.java
+++ ContaBanco/src/ContaTerminal.java
@@ -1,5 +1,19 @@
+import java.util.Scanner;
+
 public class ContaTerminal {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        Scanner scan = new Scanner(System.in);
+
+        System.out.println("Por favor digite o número da conta!");
+        int NumeroConta = scan.nextInt();
+        System.out.println("Por favor digite o número da Agência!");
+        String Agencia = scan.nextLine();
+        System.out.println("Por favor digite o nome do titular da conta!");
+        String Titular = scan.nextLine();
+        System.out.println("Por favor digite o saldo da conta!");
+        double Saldo = scan.nextDouble();
+
+        System.out.println("Olá " + Titular + ", obrigado por criar uma conta em nosso banco, sua agência é " + Agencia
+                + ", conta" + NumeroConta + " e seu saldo " + Saldo + "já está disponível para saque.");
     }
 }
